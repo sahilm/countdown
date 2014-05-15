c60abd0c776f809b919a3a92f624762434f6f132
Sahil Muthoo <sahil.muthoo@gmail.com>
2014-05-15 03:15:11 -0300

    Keep playing a tone after the timer ends

--- timer.js
+++ timer.js
@@ -1,14 +1,17 @@
 (function() {
   "use strict";
-  var start = moment.duration(15, "minutes");
+  var start = moment.duration(1, "minutes");
   var interval = moment.duration(1, "second");
   var domTarget = document.getElementById("timer");
+  var tone = document.getElementById("tone");
   var renderTimer = function() {
     domTarget.textContent = start.minutes() + "m" + " " + start.seconds() + "s";
     if (start.asSeconds() > 0) {
       start.subtract(interval);
-      setTimeout(renderTimer, interval.asMilliseconds());
+    } else {
+      tone.play();
     }
+    setTimeout(renderTimer, interval.asMilliseconds());
   };
   renderTimer();
 })();
