06880c8c72b5fea00eb58e2b19c95cc9fe9417f0
Sahil Muthoo <sahil.muthoo@gmail.com>
2014-05-19 23:31:37 -0300

    Render the time components only if relevant.

--- timer.js
+++ timer.js
@@ -1,24 +1,46 @@
 (function() {
   "use strict";
-  var start = moment.duration(minutes(), "minutes");
+  var duration = moment.duration(minutes(), "minutes");
   var interval = moment.duration(1, "second");
-  var domTarget = document.getElementById("timer");
+  var target = document.getElementById("timer");
   var tone = document.getElementById("tone");
-  var renderTimer = function() {
-    domTarget.textContent = start.minutes() + "m" + " " + start.seconds() + "s";
-    if (start.asSeconds() > 0) {
-      start.subtract(interval);
+  var timer = function() {
+    target.textContent = renderDuration(duration);
+    if (duration.asSeconds() > 0) {
+      duration.subtract(interval);
     } else {
       tone.play();
     }
-    setTimeout(renderTimer, interval.asMilliseconds());
+    setTimeout(timer, interval.asMilliseconds());
   };
 
-  renderTimer();
+  timer();
 
   function minutes() {
     var matches = window.location.search.match(/(m|minutes)=(\d+)/);
     return matches && matches[2] ? parseInt(matches[2]) : 15;
+  }
+
+  function renderDuration(duration) {
+    var hours = function() {
+      return duration.hours() + "h";
+    };
+
+    var minutes = function() {
+      return duration.minutes() + "m";
+    };
+
+    var seconds = function() {
+      return duration.seconds() + "s";
+    };
+
+    var ret = [seconds()];
+    if (duration.hours() > 0) {
+      ret.unshift(hours(), minutes());
+    } else if (duration.minutes() > 0) {
+      ret.unshift(minutes());
+    }
+    return ret.join(" ");
   }
 
 })();
